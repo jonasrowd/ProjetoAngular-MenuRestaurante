6b61575e1bc4418d682e5e2948e90810aff7143d
Jonas Machado <jonas.c.junior@capgemini.com>
2022-11-10 18:07:48 -0300

    E2E Test

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,20 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
+  navigateTo(link: string) {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getParagraphText(selector: string) {
+    return element(by.css(selector)).getText();
   }
+
+  getElement(selector: string) {
+    return element(by.css(selector));
+  }
+
+  getAllElements(selector: string) {
+    return element.all(by.css(selector));
+  }
+
 }
